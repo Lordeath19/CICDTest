f5a9288d6a5cb7a7900fba97e83f3712f20552ed
Lordeath19 <yrayhany@gmail.com>
2020-04-23 13:56:53 +0300

    Added mult

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def mult(a, b):
+    return a * b
+
+
 def add(a, b):
     return a + b
 
--- main_test.py
+++ main_test.py
@@ -1,4 +1,12 @@
-from main import add
+from main import add, mult
+
+def test_mult():
+    assert add(0, 0) == 0
+    assert add(1, 3) == 3
+    assert add(5, 12) == 60
+    assert add(3, -4) == -12
+    assert add(0, -1) == -1
+
 
 def test_add():
     assert add(0, 0) == 0
